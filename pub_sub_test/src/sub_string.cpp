845591afac16e178062897ae74dc857c6115f1bd
zhaozhongch <zhch5450@colorado.edu>
2018-11-04 23:47:45 -0700

    commit1

---
+++ pub_sub_test/src/sub_string.cpp
@@ -0,0 +1,23 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+/**
+ * This tutorial demonstrates simple receipt of messages over the ROS system.
+ */
+void chatterCallback(const std_msgs::String::ConstPtr& msg)
+{
+  ROS_INFO("I heard: [%s]", msg->data.c_str());
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "listener");
+
+  ros::NodeHandle n;
+
+  ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+
+  ros::spin();
+
+  return 0;
+}
